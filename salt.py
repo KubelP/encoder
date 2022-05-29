dc17b848e7cdafa40f65520370e9ac4611483dc0
Quantum <jakubpiergies@gmail.com>
2022-05-29 22:08:53 +0200

    small cosmetic change

--- salt.py
+++ salt.py
@@ -1,6 +1,3 @@
 '''salt (salt: bytes) necessary to encrypt and decrypt - it should be not available to unauthorized persons'''
 
-from operator import is_
-
-
 salt = b'123456789' # default salt for demo purposes - before real use need to be change
